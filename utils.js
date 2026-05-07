00b3ed5b40d04b7f969b170e0e67e082f3e443c8
wangmoods <wangmuzhi18@gmail.com>
2026-05-07 18:39:47 +0100

    parameter debugger

--- utils.js
+++ utils.js
@@ -66,7 +66,7 @@ const pre_kick_out_warning = {
 }
 
 let kick_out_warning = {}
-if (window.context == "relmed") {
+if (window.context == "pradam") {
     console.log("Relmed context detected");
     kick_out_warning = {
         type: jsPsychHtmlKeyboardResponse,
@@ -142,7 +142,7 @@ const kick_out = {
 }
 
 const create_instruction_kick_out = (task) => {
-    if (window.context == "relmed") {
+    if (window.context == "pradam") {
         return undefined;
     } else {
         return {
@@ -184,7 +184,7 @@ const create_instruction_kick_out = (task) => {
   
   // Function that checks for fullscreen
 function check_fullscreen(){
-    if (window.debug || window.context === "relmed"){
+    if (window.debug || window.context === "pradam"){
         return false
     }
 
@@ -209,9 +209,9 @@ function postToParent(message, fallback = () => {}) {
         if (window.parent && window.parent.postMessage) {
             const allowedOrigins = [
                 'http://localhost:3000',
-                'https://relmed.ac.uk',
-                'https://www.relmed.ac.uk',
-                'https://beta.relmed.ac.uk'
+                'https://pradam.org',
+                'https://www.pradam.org',
+                'https://beta.pradam.org'
             ];
 
             // Normalize a URL by removing trailing slashes
@@ -288,7 +288,7 @@ function saveDataREDCap(retry = 1, extra_fields = {}, callback = () => {}) {
 
     console.log("Data to be sent:", data_message);
 
-    if (window.context === "relmed") {
+    if (window.context === "pradam") {
         // Check if we're in a development environment
         if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
             console.log("Development mode: skipping data save to parent");
@@ -316,48 +316,49 @@ function saveDataREDCap(retry = 1, extra_fields = {}, callback = () => {}) {
         callback();
 
     } else if (window.context === "prolific") {
+        throw new Error(`This program is not ready for prolific. Please check context. Current context: ${window.context}`);
 
-        var redcap_record = JSON.stringify([{
-            record_id: window.participantID + "_" + window.module_start_time,
-            participant_id: window.participantID,
-            sitting_start_time: window.module_start_time,
-            session: window.session,
-            module: window.task,
-            data: combined_data
-        }])
+        // var redcap_record = JSON.stringify([{
+        //     record_id: window.participantID + "_" + window.module_start_time,
+        //     participant_id: window.participantID,
+        //     sitting_start_time: window.module_start_time,
+        //     session: window.session,
+        //     module: window.task,
+        //     data: combined_data
+        // }])
     
-        fetch('https://h6pgstm0f9.execute-api.eu-north-1.amazonaws.com/prod/submit', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: redcap_record
-        })
-        .then(data => {
-            if (data.status === 200) {
-                console.log('Data successfully submitted to REDCap');
-            } else {
-                console.error('Error submitting data:', data.message);
-            }
-            return data.json()
-        })
-        .then(data => {
-            console.log(data)
-            callback(); // Call the callback function if submission is successful
-        }
-        )
-        .catch(error => {
-            console.error('Error:', error);
-            if (retry > 0) {
-                console.log('Retrying to submit data...');
-                setTimeout(function(){
-                    saveDataREDCap(retry - 1);
-                }, 1000);
-            } else {
-                console.error('Failed to submit data after retrying.');
-                callback(error); // Call the callback function with the error if retries are exhausted
-            }
-        });
+        // fetch('https://h6pgstm0f9.execute-api.eu-north-1.amazonaws.com/prod/submit', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: redcap_record
+        // })
+        // .then(data => {
+        //     if (data.status === 200) {
+        //         console.log('Data successfully submitted to REDCap');
+        //     } else {
+        //         console.error('Error submitting data:', data.message);
+        //     }
+        //     return data.json()
+        // })
+        // .then(data => {
+        //     console.log(data)
+        //     callback(); // Call the callback function if submission is successful
+        // }
+        // )
+        // .catch(error => {
+        //     console.error('Error:', error);
+        //     if (retry > 0) {
+        //         console.log('Retrying to submit data...');
+        //         setTimeout(function(){
+        //             saveDataREDCap(retry - 1);
+        //         }, 1000);
+        //     } else {
+        //         console.error('Failed to submit data after retrying.');
+        //         callback(error); // Call the callback function with the error if retries are exhausted
+        //     }
+        // });
     }
 
 }
@@ -367,7 +368,7 @@ function end_experiment() {
 
     window.removeEventListener('beforeunload', preventRefresh);
 
-    if (window.context === "relmed") {
+    if (window.context === "pradam") {
         saveDataREDCap(10, {
             message: "endTask"
         });
@@ -1079,7 +1080,7 @@ const bonus_trial = {
     type: jsPsychHtmlButtonResponse,
     css_classes: ['instructions'],
     stimulus: function (trial) {
-        let event = window.context === "relmed" ? "module" : "session";
+        let event = window.context === "pradam" ? "module" : "session";
         let stimulus =  `Congratulations! You are nearly at the end of this ${event}!`      
         const total_bonus = computeTotalBonus();
         stimulus += `
